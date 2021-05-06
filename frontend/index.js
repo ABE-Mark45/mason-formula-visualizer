50363f05c5391cccd67218089c4c6c7c83e3bb3b
youssefhesham2000 <58773494+youssefhesham2000@users.noreply.github.com>
2021-05-06 15:01:45 +0200

    simple graph example
    added 2 functions for buttons functionality
    ps : not working yet

---
+++ frontend/index.js
@@ -0,0 +1,15 @@
+//var data=[[]]
+var nodes=[]
+ class Method{
+
+  checkAvailability(from,To){
+    if (nodes[from]==1||nodes[To]==1){
+      return true
+    }
+    else return false    
+  }
+  appendDot(counter,dot){
+    dot.concat();
+  }
+
+}
