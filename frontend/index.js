3afdfcabcfc71592c5d8da2cd7348178327f4146
youssefhesham2000 <58773494+youssefhesham2000@users.noreply.github.com>
2021-05-06 20:31:49 +0200

    front done
    finished adding and attaching functionality

--- frontend/index.js
+++ frontend/index.js
@@ -1,16 +1,28 @@
-//var data=[[]]
 var nodes=[]
  class Method{
-
-  checkAvailability(from,To){
-    if (nodes[from]==1||nodes[To]==1){
+ 
+  checkAvailability(from,To,counter){
+    if (from<counter&&To<counter&& from>-1&&To>-1){
+      console.log(from,To,counter)
       return true
     }
-    else return false    
+    else{
+      console.log(from,To,counter)
+      return false 
+    }
+       
   }
   appendDot(counter,dot){
-    dot.concat();
+    dot=dot.substring(0,dot.length-1)
+   dot=dot.concat(";"+counter+"}");
+   nodes[counter]=1;
+   return dot;
     //remove last char } then concat new node counter;} to dot 
+  }
+  adjustDot(from,to,dot){
+    dot=dot.substring(0,dot.length-1)
+    dot=dot.concat(";"+from+"->"+to+"}");
+    return dot;
   }
 
 }
