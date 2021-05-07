e50687f25d6416228b7fa6df31c6f663ccdfc2dc
youssefhesham2000 <58773494+youssefhesham2000@users.noreply.github.com>
2021-05-07 20:17:03 +0200

    weight added

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,10 @@
 var nodes=[]
+var vals=[]
+var smallest=0;
  class Method{
  
   checkAvailability(from,To,dot){
-    if (dot.includes(";"+from)&&dot.includes(";"+To)&&!dot.includes(";"+from+"->"+To)){
+    if (dot.includes(from)&&dot.includes(To)&&!dot.includes(";"+from+"->"+To)){
       console.log(from,To,counter)
       return true
     }
@@ -14,40 +16,69 @@ var nodes=[]
   }
   appendDot(counter,dot){
     dot=dot.substring(0,dot.length-1)
-   dot=dot.concat(";"+counter+"}");
-   var newNode=[]
+  if(counter==0){
+    dot=dot.concat(+counter+"}");
+  }
+  else{
+    dot=dot.concat(";"+counter+"}");
+  }
+    var newNode=[]
+    var newVal=[];
+    vals.push(newVal);
    nodes.push(newNode);
    return dot;
     //remove last char } then concat new node counter;} to dot 
   }
-  adjustDot(from,to,dot){
+  adjustDot(from,to,dot,value){
     dot=dot.substring(0,dot.length-1)
-    dot=dot.concat(";"+from+"->"+to+"}");
-    nodes[from].push(to);
+    dot=dot.concat(";"+from+"->"+to+"[label="+"\""+value+"\""+",arrowhead=normal]"+"}");
+    var r=[to,value]
+    nodes[from].push(r);
+    nodes[to].push([from,value])
+    //nodes[from][to].push(value);
+    
     console.log(nodes);
+    console.log(vals);
     return dot;
   }
 
   deleteEdgeFromDot(from,To,dot){
-    return  dot.replace(";"+from+"->"+To,"");
+    var value;
+    for (let i = 0; i < nodes[from].length; i++) {
+        if(nodes[from][i][0]==To){
+          value=nodes[from][i][1]
+        }
+    }
+    //console.log(";"+from+"->"+To+"[label="+"\""+value+"\""+",arrowhead=normal]")
+    return  dot.replace(";"+from+"->"+To+"[label="+"\""+value+"\""+",arrowhead=normal]","");
   }
   deleteNodeFromDot(dot,node){
-    dot=dot.replace(";"+node,"")
+   
     for (let index = 0; index < nodes[node].length; index++) {
-      dot=deleteEdgeFromDot(node,nodes[node][index],dot)
-      nodes[node].splice(index,1);
+     console.log(";"+node+"->"+nodes[node][index][0]+"[label="+"\""+nodes[node][index][1]+"\""+",arrowhead=normal]")
+     dot= dot.replace(";"+node+"->"+nodes[node][index][0]+"[label="+"\""+nodes[node][index][1]+"\""+",arrowhead=normal]","");
+      //nodes[node][index].splice(0,2);
+      console.log(nodes);
     }
     for (let i = 0; i < nodes.length; i++) {
       if (i!=node) {
         for (let j = 0; j < nodes[i].length; j++) {
-          if(nodes[i][j]==node){
-            dot= dot.replace(";"+i+"->"+nodes[i][j],"")
-            nodes[i].splice(j,1);
+          if(nodes[i][j][0]==node){
+            dot= dot.replace(";"+i+"->"+nodes[i][j][0]+"[label="+"\""+nodes[i][j][1]+"\""+",arrowhead=normal]","")
+            
+            //nodes[i][j].splice(0,2);
+            console.log(nodes);
           }
           
         }
       }
       
+    }
+    if(node==smallest){
+      dot=dot.replace(node+";"," ")
+      smallest++;
+    }else{
+    dot=dot.replace(";"+node," ")
     }
 console.log(nodes);
 return dot
