a4d705609a5d622a38c4de22aa306e3fc2f6b2a8
youssefhesham2000 <58773494+youssefhesham2000@users.noreply.github.com>
2021-05-07 04:02:35 +0200

    delete added
    delete edge functionality added
    delete node functionality added

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,8 @@
 var nodes=[]
  class Method{
  
-  checkAvailability(from,To,counter){
-    if (from<counter&&To<counter&& from>-1&&To>-1){
+  checkAvailability(from,To,dot){
+    if (dot.includes(";"+from)&&dot.includes(";"+To)&&!dot.includes(";"+from+"->"+To)){
       console.log(from,To,counter)
       return true
     }
@@ -15,14 +15,42 @@ var nodes=[]
   appendDot(counter,dot){
     dot=dot.substring(0,dot.length-1)
    dot=dot.concat(";"+counter+"}");
-   nodes[counter]=1;
+   var newNode=[]
+   nodes.push(newNode);
    return dot;
     //remove last char } then concat new node counter;} to dot 
   }
   adjustDot(from,to,dot){
     dot=dot.substring(0,dot.length-1)
     dot=dot.concat(";"+from+"->"+to+"}");
+    nodes[from].push(to);
+    console.log(nodes);
     return dot;
+  }
+
+  deleteEdgeFromDot(from,To,dot){
+    return  dot.replace(";"+from+"->"+To,"");
+  }
+  deleteNodeFromDot(dot,node){
+    dot=dot.replace(";"+node,"")
+    for (let index = 0; index < nodes[node].length; index++) {
+      dot=deleteEdgeFromDot(node,nodes[node][index],dot)
+      nodes[node].splice(index,1);
+    }
+    for (let i = 0; i < nodes.length; i++) {
+      if (i!=node) {
+        for (let j = 0; j < nodes[i].length; j++) {
+          if(nodes[i][j]==node){
+            dot= dot.replace(";"+i+"->"+nodes[i][j],"")
+            nodes[i].splice(j,1);
+          }
+          
+        }
+      }
+      
+    }
+console.log(nodes);
+return dot
   }
 
 }
